0c9c1c721adeedf6ee9c70c782d8ccd1fd3e1556
Ali <ali.khawar@zigron.com>
2017-12-13 15:47:20 +0500

    fifth quesion

---
+++ question5.py
@@ -0,0 +1,10 @@
+class question5():
+
+    def get_string(letters):
+        letters.s = input()
+
+    def print_string(letters):
+        print(letters.s.upper())
+s =question5()
+s.get_string()
+s.print_string()
