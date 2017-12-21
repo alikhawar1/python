276a2a28cca870af87a6f2ec03d7a0cf81d2a4f4
Ali <ali.khawar@zigron.com>
2017-12-21 11:53:01 +0500

    exception2

--- challenge2/question4.py
+++ challenge2/question4.py
@@ -1,11 +1,14 @@
+import sys
 import math
-c=50
-h=30
-num=[]
-
-x=[i for i in input().split(',')]
-
-for d in x:
-  num.append(str(int(math.sqrt((2*c*float(d)/h)))))
-
-print(','.join(num))
+while True:
+   try:
+      c=50
+      h=30
+      num=[]
+      x=[i for i in input().split(',')]
+      for d in x:
+         num.append(str(int(math.sqrt((2*c*float(d)/h)))))
+      print(','.join(num))
+      break
+   except ValueError:
+       print("enter valid integer numbers")
