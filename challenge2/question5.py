e73412fd693d0b3e6b538d9f40b0ea4ecf8a6a6b
Ali <ali.khawar@zigron.com>
2017-12-21 14:40:30 +0500

    exception3

--- challenge2/question5.py
+++ challenge2/question5.py
@@ -1,11 +1,17 @@
-rown = int(input('rows:'))
-coln = int(input('columns:'))
-multilist = [[0 for col in range(coln)] for row in range(rown)]
-print(multilist)
+import sys
+while True:
+   try:
+       rown = int(input('rows:'))
+       coln = int(input('columns:'))
+       multilist = [[0 for col in range(coln)] for row in range(rown)]
+       print(multilist)
 
-multilist= [[i*j for i in range(rown)] for j in range(coln)]
+       multilist= [[i*j for i in range(rown)] for j in range(coln)]
 #for row in range(rown):
 #    for col in range(coln):
 #        multilist[row][col]= row*col
 
-print(multilist)
+       print(multilist)
+       break
+   except Exception:
+       print ("enter a integer value for  rows and coloumn")
